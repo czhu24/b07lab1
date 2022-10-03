880d16b109865bbafcdbc91cb2277996a69c6b37
czhu24 <clare7.zhu@gmail.com>
2022-10-02 23:24:36 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,31 @@
+import java.io.File;
+import java.io.*;
+
 public class Driver {
-	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+	public static void main(String [] args) throws FileNotFoundException {
+		//Polynomial p = new Polynomial();
+		//System.out.println(p.evaluate(3));
+		double [] c1 = {6,5,-12,1};
+		int [] e1 = {2,0,1,5};
+		Polynomial p1 = new Polynomial(c1,e1);
+		double [] c2 = {-2,-9,22,3};
+		int [] e2 = {0,3,4,1};
+		Polynomial p2 = new Polynomial(c2,e2);
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
+		if(s.hasRoot(30.5))
 			System.out.println("1 is a root of s");
 		else
 			System.out.println("1 is not a root of s");
+		Polynomial pd = p1.multiply(p2);
+		System.out.println("pd(3) = " + pd.evaluate(3));
+		/*File file = new File("C:/Users/clare/text_poly.txt");
+		try {
+			Polynomial poly = new Polynomial(file);
+			System.out.println(poly);
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}*/
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,56 +1,142 @@
-
 import java.util.Arrays;
 import java.lang.Math;
+import java.util.Collections;
+import java.util.List;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.*;
 
 public class Polynomial {
 	public double[] coefficients;
+	public int[] exponents;
 	
 	public Polynomial() {
 		coefficients = new double[] {0};
+		exponents = new int[] {0};
 	}
 	
-	public Polynomial(double[] input) {
-		coefficients = new double[input.length];
-		for(int i=0; i<input.length; i++)
-			coefficients[i] = input[i];
+	public Polynomial(double[] coeff, int[] expo) {
+		for(int i=0; i<coeff.length; i++)
+			coefficients[i] = coeff[i];
+		for(int i=0; i<expo.length; i++)
+			exponents[i] = expo[i];
+	}
+	
+	public int find(int value, int[] given) {
+		for(int j=0; j<given.length; j++) {
+			if(value == given[j])
+				return j;
+		}
+		return -10;
 	}
 	
 	public Polynomial add(Polynomial poly2) {
-		double[] coefficient2 = poly2.coefficients;
-		int large, small;
-		if(coefficients.length > coefficient2.length) {
-			large = coefficients.length;
-			small = coefficient2.length;
+		double[] newCoeff = new double[coefficients.length + poly2.coefficients.length];
+		int[] newExpo = new int[exponents.length + poly2.exponents.length];
+		int len=0;
+		for(int i=0; i<exponents.length; i++) {
+			int j = find(exponents[i], poly2.exponents);
+			if(j != -10) {
+				newExpo[len] = exponents[i];
+				newCoeff[len] = coefficients[i] + poly2.coefficients[j];
+				len++;
+				}
+			if(find(exponents[i], newExpo) == -10) {
+				newExpo[len] = exponents[i];
+				newCoeff[len] = coefficients[i];
+				len++;
+			}
 		}
-		else {
-			large = coefficient2.length;
-			small = coefficients.length;
+		for(int k=0; k<poly2.exponents.length; k++) {
+			if(find(poly2.exponents[k], newExpo) == -10) {
+				newCoeff[len] = poly2.coefficients[k];
+				newExpo[len] = poly2.exponents[k];
+				len++;
+			}
 		}
-		double[] sum = new double[large];
-		for(int i=0; i<small; i++)
-			sum[i] = coefficients[i] + coefficient2[i];
- 		if(large != small && large == coefficients.length) {
-			for(int i=small; i<large; i++)
-				sum[i] = coefficients[i];
-		}
-		else {
-			for(int i=small; i<large; i++)
-				sum[i] = coefficient2[i];
-		}
-		return new Polynomial(sum);
+		return new Polynomial(newCoeff, newExpo);
 	}
 	
 	public double evaluate(double x) {
 		double result = 0.0;
 		for(int i=0; i<coefficients.length; i++)
-			result += coefficients[i]*Math.pow(x, i);
+			result += coefficients[i]*Math.pow(x, exponents[i]);
 		return result;
 	}
 	
 	public boolean hasRoot(double check) {
-		if(Math.abs(evaluate(check)) < Math.pow(10, -10))
+		if(Math.abs(evaluate(check)) < Math.pow(10,  -10))
 			return true;
 		else
 			return false;
 	}
+	
+	public Polynomial multiply (Polynomial poly2) {
+		double[] newCoeff = new double[coefficients.length * poly2.coefficients.length];
+		int[] newExpo = new int[exponents.length * poly2.exponents.length];
+		int len=0;
+		for(int i=0; i<exponents.length; i++) {
+			for(int j=0; j<poly2.exponents.length; j++) {
+				int index = find(exponents[i]+poly2.exponents[j], newExpo);
+				if(index == -10) {
+					newExpo[len] = exponents[i]+poly2.exponents[j];
+					newCoeff[len] = coefficients[i]+poly2.coefficients[j];
+					len++;
+				}
+				else
+					newCoeff[index] += coefficients[i];
+			}
+		}
+		return new Polynomial(newCoeff, newExpo);
+	}
+	
+	public Polynomial(File file) throws IOException {
+        Scanner scan = new Scanner(file);
+        String me2 = new String("");
+        me2 = scan.nextLine();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<me2.length(); i++) {
+        	if(i != 0 && me2.charAt(i) == '-')
+        		sb.append('+');
+        	sb.append(me2.charAt(i));
+        	if((me2.charAt(i) == '-' || me2.charAt(i) == '+') && me2.charAt(i+1) == 'x')
+        		sb.append('1');
+        	}
+        me2 = sb.toString();
+        String[] me3 = me2.split("+");
+        double[] newCoeff = new double[me3.length];
+        int[] newExpo = new int[me3.length];
+        for(int i=0; i<me3.length; i++) {
+        	if(me3[i].contains("x")) {
+        		String[] me4 = me3[i].split("x");
+        		newCoeff[i] = Double.parseDouble(me4[0]);
+        		newExpo[i] = Integer.parseInt(me4[1]);
+        	}
+        	else {
+        		newCoeff[i] = Double.parseDouble(me3[i]);
+        		newExpo[i] = 0;
+        	}
+        }
+	}
+	
+	public void saveToFile(String toPoly) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<coefficients.length; i++) {
+			if(coefficients[i] != 1)
+				sb.append(String.valueOf(coefficients[i]));
+			sb.append('x');
+			if(exponents[i] != 0)
+				sb.append(String.valueOf(exponents[i]));
+			if(coefficients[i+1] > Math.pow(10, -10))
+				sb.append('+');
+		}
+		String to_file = sb.toString();
+		PrintWriter output = new PrintWriter(toPoly);
+		output.println(to_file);
+		output.close();
+	}
 }
+
